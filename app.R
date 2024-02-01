e7553cd395d32b8e9a00a46832c8524cc21ebb91
Barret Schloerke <barret@posit.co>
2024-02-01 10:38:45 -0500

    Updated app to display all times

--- app.R
+++ app.R
@@ -17,14 +17,14 @@ ui <- fluidPage(
           selectInput("OTA1", "AO-OTA Bone", choices = c("All", "1" = "1", "2R" = "2R", "2U" = "2U", "3" = "3", "4" = "4", "6" = "6", "7" = "7", "8" = "8")),
           selectInput("OTA2", "AO-OTA Segment", choices = c("All", "1" = "1", "2" = "2", "3" = "3", "4" = "4")),
           selectInput("OTAlast", "AO-OTA Region", choices = c("All", "A" = "A", "B" = "B", "C" = "C")),
-          selectInput("Gust", "Gustilo-Anderson Classification", choices = c("All", "1" = "1", "2" = "2", "3A" = "3", "3B" = "4", "3C" = "5")),
-          selectInput("times", "End Point (days post-injury)", choices = c("90" = 90, "180" = 180, "365" = 365))
+          selectInput("Gust", "Gustilo-Anderson Classification", choices = c("All", "1" = "1", "2" = "2", "3A" = "3", "3B" = "4", "3C" = "5"))
+          # , selectInput("times", "End Point (days post-injury)", choices = c("90" = 90, "180" = 180, "365" = 365))
         )
       ),
       width = 5
     ),
     mainPanel(
-      textOutput("InfectionRisk"),
+      verbatimTextOutput("InfectionRisk"),
       width = 7
     )
   )
@@ -59,15 +59,26 @@ server <- function(input, output) {
     data <- data_filtered()
 
     out <- survfit(Surv(SSIDays, SSI) ~ 1, data = data)
-    sum.out <- summary(out, times = 90) # timing
+    txts <- lapply(c(90, 180, 365), function(times) {
+      sum.out <- summary(out, times = times)
+      paste0(
+        times, " days: ",
+        100 - round(sum.out$surv * 100, 1), "%",
+        " (95% CI, ", round(100 - sum.out$upper * 100, 1),
+        "% to ", 100 - round(sum.out$lower * 100, 1), "%) with ",
+        sum.out$n, " observations"
+      )
+    })
+    return(paste0(txts, collapse = "\n"))
+    # sum.out <- summary(out, times = 90) # timing
 
-    if (input$times == "180") {
-      sum.out <- summary(out, time = 180)
-    }
-    if (input$times == "365") {
-      sum.out <- summary(out, time = 365)
-    }
-    return(paste0(100 - round(sum.out$surv * 100, 1), "%", " (95% CI, ", round(100 - sum.out$upper * 100, 1), "% to ", 100 - round(sum.out$lower * 100, 1), "%) with ", sum.out$n, " observations"))
+    # if (input$times == "180") {
+    #   sum.out <- summary(out, time = 180)
+    # }
+    # if (input$times == "365") {
+    #   sum.out <- summary(out, time = 365)
+    # }
+    # return(paste0(100 - round(sum.out$surv * 100, 1), "%", " (95% CI, ", round(100 - sum.out$upper * 100, 1), "% to ", 100 - round(sum.out$lower * 100, 1), "%) with ", sum.out$n, " observations"))
   })
 }
 shinyApp(ui = ui, server = server)
